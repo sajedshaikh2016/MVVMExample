3797c8a86b81ccc02da297633a2a4fd464fee42f
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-01-27 12:18:32 +0530

    Initial Commit

---
+++ MVVMExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMExample
+//
+//  Created by Sajed Shaikh on 27/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
